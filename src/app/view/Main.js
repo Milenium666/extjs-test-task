776038fad7863d12364b03cb269604643ea8eed5
Milenium666 <whiteangel64rus@gmail.com>
2025-09-25 17:04:02 +0300

    feat: add filtering and product grid to Main.js

--- src/app/view/Main.js
+++ src/app/view/Main.js
@@ -12,6 +12,7 @@ Ext.define('App.view.Main', {
         region: 'north',
         xtype: 'panel',
         title: 'Главное окно',
+        height: 0,
         style: {
             background: '#007acc',
             color: 'white'
@@ -32,36 +33,88 @@ Ext.define('App.view.Main', {
                     var tabPanel = Ext.ComponentQuery.query('tabpanel[region=center]')[0];
                     var store = Ext.create('App.store.Products');
 
+                    var filterIdField = Ext.create('Ext.form.field.Text', {
+                        fieldLabel: 'ID',
+                        labelWidth: 30,
+                        width: 120,
+                        listeners: {
+                            specialkey: function (field, e) {
+                                if (e.getKey() === e.ENTER) {
+                                    applyFilters(store, filterIdField.getValue(), filterDescField.getValue());
+                                }
+                            }
+                        }
+                    });
+
+                    var filterDescField = Ext.create('Ext.form.field.Text', {
+                        fieldLabel: 'Описание',
+                        labelWidth: 70,
+                        width: 200,
+                        listeners: {
+                            specialkey: function (field, e) {
+                                if (e.getKey() === e.ENTER) {
+                                    applyFilters(store, filterIdField.getValue(), filterDescField.getValue());
+                                }
+                            }
+                        }
+                    });
+
+                    function applyFilters(store, idValue, descValue) {
+                        var filters = [];
+
+                        if (idValue !== '') {
+                            var id = parseInt(idValue, 10);
+                            if (!isNaN(id)) {
+                                filters.push({ property: 'id', value: id, operator: '=' });
+                            }
+                        }
+
+                        if (descValue !== '') {
+                            filters.push({ property: 'description', value: descValue, operator: 'like' });
+                        }
+
+                        store.clearFilter();
+                        if (filters.length > 0) {
+                            store.addFilter(filters);
+                        }
+                    }
+
+                    var grid = Ext.create('Ext.grid.Panel', {
+                        store: store,
+                        columns: [
+                            { text: 'ID', dataIndex: 'id', width: 60 },
+                            { text: 'Имя', dataIndex: 'name', width: 160 },
+                            { text: 'Описание', dataIndex: 'description', flex: 1 },
+                            { text: 'Цена', dataIndex: 'price', width: 100 },
+                            {
+                                text: 'Кол-во',
+                                dataIndex: 'quantity',
+                                width: 90,
+                                renderer: function (value) {
+                                    if (value <= 0) {
+                                        return '<div style="background-color: red; color: black; padding: 4px; width: 100%; height: 100%;">' + value + '</div>';
+                                    }
+                                    return value;
+                                }
+                            }
+                        ],
+                        dockedItems: [{
+                            xtype: 'toolbar',
+                            dock: 'top',
+                            items: [filterIdField, filterDescField]
+                        }],
+                        listeners: {
+                            itemclick: function (view, record) {
+                                Ext.create('App.view.ProductCard').show(record);
+                            }
+                        }
+                    });
+
                     tabPanel.add({
                         title: 'Товары',
                         layout: 'fit',
                         closable: true,
-                        items: [{
-                            xtype: 'grid',
-                            store: store,
-                            columns: [
-                                { text: 'ID', dataIndex: 'id', width: 60 },
-                                { text: 'Имя', dataIndex: 'name', width: 160 },
-                                { text: 'Описание', dataIndex: 'description', flex: 1 },
-                                { text: 'Цена', dataIndex: 'price', width: 100 },
-                                {
-                                    text: 'Кол-во',
-                                    dataIndex: 'quantity',
-                                    width: 90,
-                                    renderer: function (value) {
-                                        if (value <= 0) {
-                                            return '<div style="background-color: red; color: black; padding: 4px; width: 100%; height: 100%;">' + value + '</div>';
-                                        }
-                                        return value;
-                                    }
-                                }
-                            ],
-                            listeners: {
-                                itemclick: function (view, record) {
-                                    Ext.create('App.view.ProductCard').show(record);
-                                }
-                            }
-                        }]
+                        items: [grid]
                     });
 
                     tabPanel.setActiveTab(tabPanel.items.length - 1);
